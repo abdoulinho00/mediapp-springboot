63497e2e423772d09969b864072219ac79aa61f3
abderrahmane el bardai <elbardai.a@gmail.com>
2017-07-14 00:49:31 -0400

    adding view visit details

--- src/main/java/com/aelbardai/web/VisitController.java
+++ src/main/java/com/aelbardai/web/VisitController.java
@@ -6,12 +6,21 @@ import com.aelbardai.patient.service.VisitService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Controller for medical visits : {General , nutrition , Esthetic} : add/ edit list / delete
@@ -81,14 +90,14 @@ public class VisitController {
     }
 
     @PostMapping("/add/general")
-    public  ModelAndView addGeneralVisist(@ModelAttribute("generalVisit")GeneralVisit generalVisit, BindingResult bindingResult, Model model){
+    public  ModelAndView addGeneralVisit(@ModelAttribute("generalVisit")GeneralVisit generalVisit, BindingResult bindingResult, Model model){
         log.info("trying to save object : {}", generalVisit);
         visitService.addVisit(generalVisit);
         return new ModelAndView("redirect:/patients");
     }
 
     @PostMapping("/add/nutrition")
-    public  ModelAndView addNutritionVisist(
+    public  ModelAndView addNutritionVisit(
             @RequestParam("beforeImage") MultipartFile beforeImage,
             @RequestParam("afterImage") MultipartFile afterImage,
             @ModelAttribute("nutritionVisit") NutritionVisit nutritionVisit,
@@ -99,9 +108,67 @@ public class VisitController {
     }
 
     @PostMapping("/add/esthetic")
-    public  ModelAndView addEstheticVisist(@ModelAttribute("estheticVisit") EstheticVisit estheticVisit, BindingResult bindingResult, Model model){
+    public  ModelAndView addEstheticVisit(@ModelAttribute("estheticVisit") EstheticVisit estheticVisit, BindingResult bindingResult, Model model){
         log.info("trying to save object : {}");
         visitService.addVisit(estheticVisit);
         return new ModelAndView("redirect:/patients");
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView viewVisitDetails(@PathVariable("id") Long id){
+        Visit visit = visitService.getVisitById(id);
+        if(visit == null){
+            return new ModelAndView("error/404" , "message" , "No visit founf for your request");
+        }
+        else{
+            return new ModelAndView("visits/details" , "visit" , visit);
+        }
+    }
+
+    @GetMapping("/image/{id}")
+    @ResponseBody
+    public FileSystemResource serveFile(HttpServletRequest request, HttpServletResponse response , @PathVariable("id") Long visitId, @RequestParam("image") String type) {
+
+        Visit visit = visitService.getVisitById(visitId);
+        String path = null;
+        switch (type){
+            case "before" : if(visit instanceof NutritionVisit){path= ((NutritionVisit) visit).getBeforePath();}
+                            break;
+            case "after" : if(visit instanceof NutritionVisit){path= ((NutritionVisit) visit).getAfterPath();}
+                            break;
+            case "left" : if(visit instanceof EstheticVisit){path= ((EstheticVisit) visit).getLeftProfilePath();}
+                break;
+            case "right" : if(visit instanceof EstheticVisit){path= ((EstheticVisit) visit).getRightProfilePath();}
+                break;
+            case "face" : if(visit instanceof EstheticVisit){path= ((EstheticVisit) visit).getFacePath();}
+                break;
+            default: break;
+
+        }
+
+            if(path != null) {
+                try {
+
+
+                    //response.setContentType("im");
+                    //response.setHeader("Content-Disposition", String.format("inline; filename=%s.%s",fileElement.getName(),fileElement.getExtension()));
+                    //return new FileSystemResource(file);
+                    response.setContentType("image/jpg");
+                    response.getOutputStream().write(Files.readAllBytes(Paths.get(path)));
+                    //return new FileSystemResource(new File(path));
+                }catch(IOException ex){
+                    log.error("couldn't write in output stream");
+                    return null;
+                }
+                catch(Exception ex){
+                    log.error("something wrong with my file");
+                }
+            }
+            else{
+                return null;
+            }
+
+
+        return null;
     }
 }
