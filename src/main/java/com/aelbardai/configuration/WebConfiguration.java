aa8f27bc87bf2b4e24a5a1014dcb59845a965e13
abderrahmane el bardai <elbardai.a@gmail.com>
2017-07-27 14:51:08 -0400

    adding multiple themes support

--- src/main/java/com/aelbardai/configuration/WebConfiguration.java
+++ src/main/java/com/aelbardai/configuration/WebConfiguration.java
@@ -2,11 +2,16 @@ package com.aelbardai.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.context.ThemeSource;
+import org.springframework.ui.context.support.ResourceBundleThemeSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ThemeResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.theme.CookieThemeResolver;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
 
 import java.util.Locale;
 
@@ -29,10 +34,33 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         interceptor.setParamName("lang");
         return interceptor;
     }
+    /*  Theme resolver   */
+    @Bean
+    public ThemeSource themeSource() {
+        ResourceBundleThemeSource source = new ResourceBundleThemeSource();
+        source.setBasenamePrefix("static/assets/themes/");
+        return source;
+    }
+    @Bean
+    public ThemeResolver themeResolver(){
+        CookieThemeResolver resolver = new CookieThemeResolver();
+        resolver.setCookieMaxAge(2400);
+        resolver.setCookieName("mytheme");
+        resolver.setDefaultThemeName("default");
+        return resolver;
+    }
+
+    @Bean
+    ThemeChangeInterceptor themeChangeInterceptor(){
+        ThemeChangeInterceptor themeInterceptor = new ThemeChangeInterceptor();
+        themeInterceptor.setParamName("theme");
+        return themeInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         super.addInterceptors(registry);
         registry.addInterceptor(localeChangeInterceptor());
+        registry.addInterceptor(themeChangeInterceptor());
     }
 }
